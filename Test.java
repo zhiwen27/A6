7bd73bf6eca3fc79bc0d7cf202b42ad3f81a61ee
zhiwen27 <157720245+zhiwen27@users.noreply.github.com>
2024-10-17 22:55:52 -0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,60 @@
+public class Test{
+    public static void Maze(int row, int col, int desX, int desY, int currentX, int currentY, boolean[][] canGo, boolean reach){
+        if (reach){
+            return;
+        }
+        if ((desX == currentX) && (desY == currentY)){
+            System.err.println("Arrive at destination.");
+            reach = true;
+            return;
+        }
+        if (canGo[currentX][currentY] == false){
+            System.err.println("You're at a wall/ you've visited here!");
+            return;
+        }
+        else{
+            canGo[currentX][currentY] = false;
+            //  Go north
+            if (((currentY + 1) < col) && ((canGo[currentX][currentY + 1] == true))){
+                System.err.println("Now at: (" + currentX + "," + (currentY + 1) + ").");
+                Maze(row, col, desX, desY, currentX, currentY + 1, canGo, reach);
+            }
+            // Go south
+            if (((currentY - 1) >= 0) && ((canGo[currentX][currentY - 1] == true))){
+                if ((currentY + 1) < col){
+                    canGo[currentX][currentY + 1] = false;    
+                }
+                System.err.println("Now at: (" + currentX + "," + (currentY - 1) + ").");
+                Maze(row, col, desX, desY, currentX, currentY - 1, canGo, reach);
+            }
+            // Go east
+            if (((currentX + 1) < row) && (canGo[currentX + 1][currentY] == true)){
+                if (((currentY - 1) >= 0)){
+                    canGo[currentX][currentY - 1] = false;
+                }
+                System.err.println("Now at: (" + (currentX + 1)+ "," + currentY + ").");
+                Maze(row, col, desX, desY,currentX + 1, currentY, canGo, reach);
+            }
+            // Go west
+            if (((currentX - 1) >= 0) && (canGo[currentX - 1][currentY] == true)){
+                if (((currentX + 1) < row)){
+                    canGo[currentX + 1][currentY] = false;
+                }
+                System.err.println("Now at: (" + (currentX - 1)+ "," + currentY + ").");
+                Maze(row, col, desX, desY, currentX - 1, currentY, canGo, reach);
+            }
+            else{
+                System.err.println("Failure!");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        boolean[][] canGo = {{true,true,true},{false,true,true},{true,true,true}};
+        int desX = 2;
+        int desY = 0;
+        int startX = 0;
+        int startY = 1;
+        Test.Maze(3, 3, desX, desY, startX, startY, canGo,false);
+
+    }
+}
