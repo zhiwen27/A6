45dc33b2d40125bcf9981877bc3bc456c071e3fb
Zhiwen Ji <zji@smith.edu>
2024-10-18 20:31:45 -0400

    good!

--- Test.java
+++ Test.java
@@ -1,48 +1,53 @@
-import java.util.*;
 public class Test{
-    public static ArrayList<Point> Maze(ArrayList<Point> route, int row, int col, int desX, int desY, int currentX, int currentY, boolean[][] canGo){
+    public static boolean Maze(int row, int col, int desX, int desY, int currentX, int currentY, boolean[][] canGo, boolean reach){
         if ((currentX == desX) && (currentY == desY)){
             System.err.println("Arrive at destination.");
-            return route;
+            reach = true;
+            return reach;
         }
         if (canGo[currentX][currentY] == false){
             System.err.println("You're at a wall/ you've visited here!");
-            return route;
+            return reach;
         }
         canGo[currentX][currentY] = false;
         //  Go north
         if (((currentX - 1) >= 0) && (canGo[currentX - 1][currentY] == true)){
-            System.err.println("From: (" + currentX+ "," + currentY + ").");
-            System.err.println("Now at: (" + (currentX - 1)+ "," + currentY + ").");
-            route.add(new Point(currentX - 1, currentY));
-            Maze(route, row, col, desX, desY, currentX - 1, currentY, canGo);
+            if(!reach){
+                System.err.println("From: (" + currentX+ "," + currentY + ").");
+                System.err.println("Now at: (" + (currentX - 1)+ "," + currentY + ").");
+                reach = Maze(row, col, desX, desY, currentX - 1, currentY, canGo, reach);
+            }
         }
         // Go south
         if (((currentX + 1) < row) && (canGo[currentX + 1][currentY] == true)){
-            System.err.println("From: (" + currentX+ "," + currentY + ").");
-            System.err.println("Now at: (" + (currentX + 1)+ "," + currentY + ").");
-            route.add(new Point(currentX + 1, currentY));
-            Maze(route,row, col, desX, desY,currentX + 1, currentY, canGo);
+            if(!reach){
+                System.err.println("From: (" + currentX+ "," + currentY + ").");
+                System.err.println("Now at: (" + (currentX + 1)+ "," + currentY + ").");
+                reach = Maze(row, col, desX, desY,currentX + 1, currentY, canGo, reach);
+            }
         }
         // Go east
         if (((currentY + 1) < col) && ((canGo[currentX][currentY + 1] == true))){
-            System.err.println("From: (" + currentX+ "," + currentY + ").");
-            System.err.println("Now at: (" + currentX + "," + (currentY + 1) + ").");
-            route.add(new Point(currentX, currentY + 1));
-            Maze(route,row, col, desX, desY, currentX, currentY + 1, canGo);
+            if(!reach){
+                System.err.println("From: (" + currentX+ "," + currentY + ").");
+                System.err.println("Now at: (" + currentX + "," + (currentY + 1) + ").");
+                reach = Maze(row, col, desX, desY, currentX, currentY + 1, canGo, reach);
+            }
         }
         // Go west
         if (((currentY - 1) >= 0) && ((canGo[currentX][currentY - 1] == true))){
-            System.err.println("From: (" + currentX+ "," + currentY + ").");
-            System.err.println("Now at: (" + currentX + "," + (currentY - 1) + ").");
-            route.add(new Point(currentX, currentY - 1));
-            Maze(route, row, col, desX, desY, currentX, currentY - 1, canGo);
+            if(!reach){
+                System.err.println("From: (" + currentX+ "," + currentY + ").");
+                System.err.println("Now at: (" + currentX + "," + (currentY - 1) + ").");
+                reach = Maze(row, col, desX, desY, currentX, currentY - 1, canGo, reach);
+            }
         }
         else{
-            System.err.println("Fail to find a way at: (" + currentX + "," + currentY + ").");
-            //return route;
+            if (!reach){
+                System.err.println("Fail to find a way at: (" + currentX + "," + currentY + ").");
+            }
         }
-        return route;
+        return reach;
     }
     public static void main(String[] args) {
         boolean[][] canGo = {{true,true,true},{false,true,true},{true,true,true}};
@@ -70,15 +75,6 @@ public class Test{
             System.err.println();
         }
         System.err.println("Start at: (" + startX + "," + startY + ").");
-        ArrayList<Point> route = new ArrayList<>();
-        route.add(new Point(startX, startY));
-        route = Test.Maze(route, row, col, desX, desY, startX, startY, canGo);
-        for(Point p: route){
-            if ((p.x == desX) && (p.y == desY)){
-                System.err.println(p);
-                break;
-            }
-            System.err.print(p + "->");
-        }
+        Test.Maze(row, col, desX, desY, startX, startY, canGo, false);
     }
 }
